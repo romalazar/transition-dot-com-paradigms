dbc839706092f809719a244e51b1f00a3f76930c
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-12-06 00:26:41 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,52 @@
+        }
+        int[] data = new int[10];
+        }
+        for (int i = 0; i < 10; i++) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+    }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+
+
+def generate_random_data():
+if __name__ == "__main__":
+import random
+def main():
+    return data
+    main()
+    for item in data:
+        print(f"Random Number: {item}")
+
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+
+    data = generate_random_data()
+
+def main():
+def generate_random_data():
+
+if __name__ == "__main__":
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
+import randomif __name__ == "__main__":
+
+    for item in data:
+import random
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def generate_random_data():
+def main():
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    main()
